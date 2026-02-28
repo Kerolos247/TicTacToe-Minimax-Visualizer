d76f5298bc45d2f4c9a89e300e63d51aa6d89d10
Kerolos247 <kerolos.adel754@gmail.com>
2026-02-28 20:16:44 +0200

    Separate UI from Game Logic

--- WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/Form1.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
-
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace WindowsFormsApp8
 {
@@ -19,171 +14,75 @@ namespace WindowsFormsApp8
         Node root;
         TreeDrawer drawer = new TreeDrawer();
         SoundPlayer player = new SoundPlayer();
-        
+        Timer timer = new Timer();
 
-        Button[,] btns = new Button[3, 3];
-        Panel panel1;
-        Button btnShowTree;
-        Button btnRestart_GAME=new Button();
-
-        Timer timer=new Timer();
-
-        bool isStop= false;
+        GameUIManager ui; 
+        bool isStop = false;
 
         public Form1()
         {
-            this.Width = 1200;
-            this.Height = 700;
-            this.Text = "Tic Tac Toe + Minimax Visualization";
+            InitializeComponent();
+            ui = new GameUIManager(this);
+            this.Load += Form1_Load;
+           
+            AttachEvents();
+
+            player.SoundLocation = Path.Combine(Application.StartupPath, "gameover.wav");
+            timer.Interval = 500;
             timer.Tick += Timer_Tick;
             timer.Start();
-            player.SoundLocation = @"D:\BackEnd_Clininc\WindowsFormsApp8\WindowsFormsApp8\gameover.wav";
-            CreateBoardButtons();
-            CreateShowTreeButton();
-            CreatePanelForTree();
-            btnRestart_GAME.Name = "btnRestart_GAME";
-            btnRestart_GAME.Text = "Restart Game";
-            btnRestart_GAME.Size = new Size(150, 50);
-            btnRestart_GAME.Location = new Point(20,500);  
-            btnRestart_GAME.BackColor = Color.LightSkyBlue;
-            btnRestart_GAME.ForeColor = Color.White;
-            btnRestart_GAME.Font = new Font("Segoe UI", 14, FontStyle.Bold);
-            btnRestart_GAME.FlatStyle = FlatStyle.Flat;
-            btnRestart_GAME.FlatAppearance.BorderSize = 0;   
-            btnRestart_GAME.Click += btn_Restart_GAME;
 
-       
-            this.Controls.Add(btnRestart_GAME);
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    board[i, j] = "";
+            ResetBoardData();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            if (minimax.CheckWinner(board).Winner!="")
-            {
-                
-                isStop = true;
-                timer.Stop();
-                //player.Play();
-                foreach (var c in minimax.CheckWinner(board).Squares)
-                {
-                    btns[c.Item1, c.Item2].BackColor = Color.Yellow;
-                }
-              
-            }
-        }
-        private void btn_Restart_GAME(object sender, EventArgs e)
-        {
-            
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    board[i, j] = "";
-
            
-            for (int r = 0; r < 3; r++)
-            {
-                for (int c = 0; c < 3; c++)
-                {
-                    Button btn = this.Controls[$"btn{r}{c}"] as Button;
-
-                    if (btn != null)
-                    {
-                        btn.Text = "";                        
-                        btn.BackColor = Color.White;         
-                        btn.Enabled = true;                 
-                    }
-                }
-            }
-
-            
-            root = null;
-            isStop = false;
-            timer.Start();
-            panel1.Refresh();
         }
 
-
-        private void CreateBoardButtons()
+        private void AttachEvents()
         {
-            int x = 20, y = 20;
-            int size = 80;
-
+           
             for (int i = 0; i < 3; i++)
-            {
                 for (int j = 0; j < 3; j++)
-                {
-                    Button b = new Button();
-                    b.Name = $"btn{i}{j}";
-                    b.Text = "";
-                    b.Width = size;
-                    b.Height = size;
-                    b.Font = new Font("Arial", 18, FontStyle.Bold);
-                    b.Location = new Point(x + j * size, y + i * size);
-                    b.Click += btn_Click;
+                    ui.Btns[i, j].Click += btn_Click;
 
-                    btns[i, j] = b;
-                    this.Controls.Add(b);
-                }
-            }
+            ui.BtnRestart.Click += btn_Restart_GAME;
+            ui.BtnShowTree.Click += (s, e) => ui.TreePanel.Refresh();
+            ui.TreePanel.Paint += panel1_Paint;
         }
 
-        
-        private void CreateShowTreeButton()
+        private void ResetBoardData()
         {
-            btnShowTree = new Button();
-            btnShowTree.Text = "Show Tree";
-            btnShowTree.Width = 200;
-            btnShowTree.Height = 40;
-            btnShowTree.Font = new Font("Arial", 12, FontStyle.Bold);
-            btnShowTree.Location = new Point(20, 300);
-
-            btnShowTree.Click += btnShowTree_Click;
-
-            this.Controls.Add(btnShowTree);
-        }
-
-        private void CreatePanelForTree()
-        {
-            panel1 = new Panel();
-            panel1.Name = "panel1";
-            panel1.Width = 800;
-            panel1.Height = 600;
-            panel1.BackColor = Color.White;
-            panel1.Location = new Point(350, 20);
-            panel1.BorderStyle = BorderStyle.FixedSingle;
-
-            panel1.Paint += panel1_Paint;
-
-            this.Controls.Add(panel1);
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    board[i, j] = "";
         }
 
         private void btn_Click(object sender, EventArgs e)
         {
-            if(isStop)
+            if (isStop)
                 return;
+
             Button btn = sender as Button;
-            int r = int.Parse(btn.Name[3].ToString());
-            int c = int.Parse(btn.Name[4].ToString());
-           
-            if (btn.Text != "") 
-                return;
+            int r = int.Parse(btn.Name.Substring(3, 1));
+            int c = int.Parse(btn.Name.Substring(4, 1));
 
-            btn.Text = "O";
+            if (board[r, c] != "") return;
+
             board[r, c] = "O";
+            ui.UpdateCell(r, c, "O");
 
-           
             ComputerPlay();
         }
 
-       
         void ComputerPlay()
         {
-            root = minimax.BuildTree(board, true);
+            if (minimax.CheckWinner(board).Winner != "")
+                return;
 
-            int bestVal = -999999999;
-            int bestR = 0, bestC = 0;
+            root = minimax.BuildTree(board, true);
+            int bestVal = -1000, bestR = 0, bestC = 0;
 
             for (int i = 0; i < 3; i++)
             {
@@ -194,33 +93,44 @@ namespace WindowsFormsApp8
                         board[i, j] = "X";
                         var val = minimax.BuildTree(board, false).Value;
                         board[i, j] = "";
-
-                        if (val > bestVal)
-                        {
-                            bestVal = val;
-                            bestR = i;
-                            bestC = j;
-                        }
+                        if (val > bestVal) { bestVal = val; bestR = i; bestC = j; }
                     }
                 }
             }
 
             board[bestR, bestC] = "X";
-            btns[bestR, bestC].Text = "X";
+            ui.UpdateCell(bestR, bestC, "X");
         }
 
-        private void btnShowTree_Click(object sender, EventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            panel1.Refresh();
+            CheckGameOver();
         }
+        private void CheckGameOver()
+        {
+            var result = minimax.CheckWinner(board);
+            if (result.Winner != "")
+            {
+                isStop = true;
+                timer.Stop();
+                player.Play();
+                ui.HighlightWinner(result.Squares);
+            }
+        }
+
+        private void btn_Restart_GAME(object sender, EventArgs e)
+        {
+            ResetBoardData();
+            ui.ResetUI();
+            root = null;
+            isStop = false;
+            timer.Start();
+        }
+
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             if (root != null)
-                drawer.DrawTree(e.Graphics, root, panel1.Width / 2, 20);
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
+                drawer.DrawTree(e.Graphics, root, ui.TreePanel.Width / 2, 20);
         }
     }
 }
-
---
+++ WindowsFormsApp8/UI/GameUIManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public class GameUIManager
+    {
+        private Form _parentForm;
+        public Button[,] Btns = new Button[3, 3];
+        public Panel TreePanel;
+        public Button BtnRestart;
+        public Button BtnShowTree;
+
+        public GameUIManager(Form form)
+        {
+            _parentForm = form;
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            _parentForm.Width = 1200;
+            _parentForm.Height = 700;
+            _parentForm.Text = "Tic Tac Toe + Minimax Visualization";
+
+           
+            int x = 20, y = 20, size = 80;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button b = new Button
+                    {
+                        Name = $"btn{i}{j}",
+                        Width = size,
+                        Height = size,
+                        Font = new Font("Arial", 18, FontStyle.Bold),
+                        Location = new Point(x + j * size, y + i * size),
+                        BackColor = Color.White
+                    };
+                    Btns[i, j] = b;
+                    _parentForm.Controls.Add(b);
+                }
+            }
+
+           
+            BtnShowTree = new Button
+            {
+                Text = "Show Tree",
+                Width = 200,
+                Height = 40,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                Location = new Point(20, 300)
+            };
+            _parentForm.Controls.Add(BtnShowTree);
+
+           
+            BtnRestart = new Button
+            {
+                Text = "Restart Game",
+                Size = new Size(150, 50),
+                Location = new Point(20, 500),
+                BackColor = Color.LightSkyBlue,
+                ForeColor = Color.White,
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                FlatStyle = FlatStyle.Flat
+            };
+            _parentForm.Controls.Add(BtnRestart);
+
+          
+            TreePanel = new Panel
+            {
+                Width = 800,
+                Height = 600,
+                BackColor = Color.White,
+                Location = new Point(350, 20),
+                BorderStyle = BorderStyle.FixedSingle
+            };
+            _parentForm.Controls.Add(TreePanel);
+        }
+
+        public void UpdateCell(int r, int c, string text)
+        {
+            Btns[r, c].Text = text;
+        }
+
+       
+        public void HighlightWinner(System.Collections.Generic.List<(int row, int col)> squares)
+        {
+            foreach (var sq in squares)
+            {
+                
+                Btns[sq.row, sq.col].BackColor = Color.Yellow;
+            }
+        }
+        public void ResetUI()
+        {
+            foreach (var btn in Btns)
+            {
+                btn.Text = "";
+                btn.BackColor = Color.White;
+                btn.Enabled = true;
+            }
+            
+        }
+    }
+}
